f4bcf7b766f770d468ced27e9b593fa30caaf0b6
Y45K0VL3V <yakovlevvadim666@gmail.com>
2023-02-02 00:43:30 +0300

    Notes app start development
    * created project
    * add .gitignore

---
+++ yakov.Notes/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace yakov.Notes;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ yakov.Notes/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace yakov.Notes;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
