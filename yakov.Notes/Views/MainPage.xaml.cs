42d8df8a3d019da071f0c19025001466a42834ee
Y45K0VL3V <yakovlevvadim666@gmail.com>
2023-02-06 16:12:05 +0300

    Created Views folder
    * created new pages
    * moved MainPage to Views folder

---
+++ yakov.Notes/Views/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.ObjectModel;
+using yakov.Notes.ViewModel;
+
+namespace yakov.Notes;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage(MainPageVM vm)
+	{
+		InitializeComponent();
+		BindingContext = vm;
+	}
+}
+
---
+++ yakov.Notes/Views/RegisterPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace yakov.Notes.Views;
+
+public partial class RegisterPage : ContentPage
+{
+	public RegisterPage()
+	{
+		InitializeComponent();
+	}
+}
