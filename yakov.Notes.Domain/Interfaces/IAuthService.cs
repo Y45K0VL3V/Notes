7d3bd2fb3ea1f730c928616ac8a941e31137cd8d
Y45K0VL3V <yakovlevvadim666@gmail.com>
2023-02-07 13:38:22 +0300

    Created base model for Firebase authorization

---
+++ yakov.Notes.Domain/Interfaces/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yakov.Notes.Domain.Interfaces
+{
+    public interface IAuthService
+    {
+        public Task<bool> SignInAsync();
+        public Task<bool> SignUpAsync();
+    }
+}
---
+++ yakov.Notes.Services/FirebaseAuthService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using yakov.Notes.Domain.Interfaces;
+
+namespace yakov.Notes.Services
+{
+    public class FirebaseAuthService : IAuthService
+    {
+        public Task<bool> SignInAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SignUpAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
