927b313a552b5152dffe73b93756dfea44ae65d5
Y45K0VL3V <yakovlevvadim666@gmail.com>
2023-02-07 22:42:38 +0300

    Implemented registration page & VM

--- yakov.Notes/ViewModel/RegisterPageVM.cs
+++ yakov.Notes/ViewModel/RegisterPageVM.cs
@@ -1,13 +1,38 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+using yakov.Notes.Domain.Interfaces;
 
 namespace yakov.Notes.ViewModel
 {
     public partial class RegisterPageVM : ObservableObject
     {
+        public RegisterPageVM(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        private IAuthService _authService;
+
+        [ObservableProperty]
+        private string _userEmail;
+
+        [ObservableProperty]
+        private string _userPassword;
+
+        [RelayCommand]
+        private async void SignUp()
+        {
+            try
+            {
+                var token = await _authService.SignUpAsync(_userEmail, _userPassword);
+                if (token is not null)
+                    await App.Current.MainPage.DisplayAlert("Alert", "User Registered successfully", "OK");
+            }
+            catch (Exception ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Alert", ex.Message, "OK");
+                throw;
+            }
+        }
     }
 }
