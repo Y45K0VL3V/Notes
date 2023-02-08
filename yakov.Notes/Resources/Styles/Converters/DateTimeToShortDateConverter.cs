532681526d74c10d95886c4885e326a847bd7cb5
Y45K0VL3V <yakovlevvadim666@gmail.com>
2023-02-08 15:24:29 +0300

    Updated MainPage
    * replaced installed nuget package with CollectionView with own
    * created converter to format DateTime of LastTimeModified

---
+++ yakov.Notes/Resources/Styles/Converters/DateTimeToShortDateConverter.cs
@@ -0,0 +1,35 @@
+﻿using Kotlin.Jvm.Functions;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Android.Provider.Telephony;
+
+namespace yakov.Notes.Resources.Styles.Converters
+{
+    class DateTimeToShortDateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime? dateTime = value as DateTime?;
+            if (!dateTime.HasValue) 
+                return null;
+
+            if ((DateTime.Now - dateTime.Value).TotalDays < 1)
+                return dateTime.Value.ToString("HH:mm");
+
+            if (DateTime.Now.Year == dateTime.Value.Year)
+                return dateTime.Value.ToString("dd MMM");
+            else
+                return dateTime.Value.ToString("dd MMM yyyy");
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
